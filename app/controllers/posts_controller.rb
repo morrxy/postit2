0aeaf1d12b7a55e3995ef622d54e91d53d03ce6b
morrxy <morrxy@gmail.com>
2013-11-05 23:43:13 +0800

    a

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,22 +4,43 @@ class PostsController < ApplicationController
   end
 
   def show
-    # binding pry
     @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
   def create
-    binding.pry
-    @post = Post.create(params[:my_title])
+    @post = Post.create(post_params)
+
+    if @post.save
+      flash[:notice] = 'Your post was created'
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      flash[:notice] = 'The post was updated'
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit!
   end
 
 end
