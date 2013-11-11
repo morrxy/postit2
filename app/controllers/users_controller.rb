03d843347de3fdef24a4d29add06d04d0ebca86a
morrxy <morrxy@gmail.com>
2013-11-11 23:51:36 +0800

    m

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:notice] = 'User was created'
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = 'User was updated'
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
+
+  get 'register', to: 'users#new'
+
   resources :posts, except: :destroy do
     resources :comments, only: [:create]
   end
   resources :categories, only: [:new, :create, :show]
+  resources :users
 end
